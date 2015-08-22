92e99edf773eecf4e88a8372d8d578c479e5df6f
GitNiko <galaxis.ling@gmail.com>
2015-08-22 09:23:10 +0800

    前端展示

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // main loading
-angular.module('app', ['ui.router', 'ngMaterial'])
+angular.module('app', ['ui.router', 'ngMaterial', 'app.ctrl', 'app.service'])
 .constant('KeyNames',{'domain': 'http://wechat.qianmi.com'})
 .config(['$httpProvider', '$stateProvider', '$urlRouterProvider', 'KeyNames',
 function($httpProvider, $stateProvider, $urlRouterProvider, KeyNames) {
@@ -8,9 +8,32 @@ function($httpProvider, $stateProvider, $urlRouterProvider, KeyNames) {
   $stateProvider
     .state('index', {
       url: "",
-      controller: 'IndexCtrl',
-      templateUrl: 'template/index.html'
-  });
+      views: {
+          'main': {
+              controller: 'IndexCtrl',
+              templateUrl: 'template/index.html'
+          }
+      }
+  })
+  .state('main', {
+    url: "/main",
+    abstract: true,
+    views: {
+        'main': {
+            controller: 'MainCtrl',
+            templateUrl: 'template/main.html'
+        }
+    }
+})
+.state('main.baseinfo', {
+  url: "/baseinfo/:id",
+  views: {
+      'pane': {
+          controller: 'BaseInfoCtrl',
+          templateUrl: 'template/baseinfo.html'
+      }
+  }
+});
   //   .state('home', {
   //     url: "/home",
   //     views: {
@@ -54,49 +77,4 @@ function($httpProvider, $stateProvider, $urlRouterProvider, KeyNames) {
   //       'naviview': {templateUrl: 'template/navi.logon.html'}
   //     }
   // });
-}])
-.controller('IndexCtrl',['$scope', '$http', 'KeyNames', '$mdDialog',
-function($scope, $http, KeyNames, $mdDialog){
-    // $http.get('www.baidu.com');
-    var DialogController = function($scope, $http, $mdDialog) {
-        $http.get(KeyNames.domain + '/oauth/req')
-        .then(function(authLink){
-            $scope.authLink = authLink.data.url;
-            // window.open('http://wechat.qianmi.com/oauth/req');
-        });
-        $scope.hide = function() {
-            $mdDialog.hide();
-        };
-        $scope.cancel = function() {
-            $mdDialog.cancel();
-        };
-        $scope.answer = function(answer) {
-            $mdDialog.hide(answer);
-        };
-    };
-    $scope.showAuthDialog = function(ev) {
-        $mdDialog.show({
-          controller: DialogController,
-          templateUrl: 'template/auth.pop.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose:true
-        })
-        .then(function(answer) {
-          $scope.status = 'You said the information was "' + answer + '".';
-        }, function() {
-          $scope.status = 'You cancelled the dialog.';
-        });
-    };
-}])
-.controller('MainCtrl', ['$scope', '$http', 'KeyNames',
-function($scope, $http, KeyNames){
-    // $http.get('www.baidu.com');
-    console.log('asdasd');
-    $scope.auth = function() {
-        $http.get(KeyNames.domain + '/auth/req')
-        .then(function(authLink){
-            window.open('');
-        });
-    };
 }]);
